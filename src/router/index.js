a1333bfed813d7689145fbaebde147c30b17afb4
mbesagil <m.besagil@gmail.com>
2022-07-24 12:56:09 +0300

    location and markers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Usermaps from '@/maps/Usermaps'
 import CloseBuy from '@/maps/CloseBuy'
 
