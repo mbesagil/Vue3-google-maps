2d85094ff0c72fa40d10caa79d75e2ae820c6a56
mbesagil <m.besagil@gmail.com>
2022-07-24 12:49:12 +0300

    location and markers

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Usermaps from '@/maps/Usermaps'
+import CloseBuy from '@/maps/CloseBuy'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Usermaps
+    },
+    {
+      path: '/close-buy',
+      component: CloseBuy
+    }
+  ]
+})
